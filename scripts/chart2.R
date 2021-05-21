b112964024c121a4958dc0ec97e8c0df516bda22
Saketh Nimmagadda <sakethn@cs.washington.edu>
2021-05-20 19:22:12 -0700

    Updated chart 2

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -45,12 +45,44 @@ get_date <- function(date) {
 
 all_coins <- all_coins %>% mutate(Date = get_date(Date))
 
-volume_over_time <- ggplotly(ggplot(data = all_coins) +
-                              geom_point(mapping = aes(x = Date, y = Volume, color = Symbol)) +
-                              ggtitle("Daily Volume over Time") +
-                              xlab("Time") +
-                              ylab("Volume ($ Transacted)") +
-                               scale_x_date(date_labels = "%b-%d-%Y"))
+mkt_bar_graph <- function(coins) {
+  max_coin_mktcap <- coins %>%
+    group_by(Name) %>%
+    mutate(max_mkt_cap = round(max(Marketcap), 2)) %>%
+    slice(1) %>%
+    select(Symbol, max_mkt_cap)
+  mkt_max <- ggplotly(ggplot(data = max_coin_mktcap) +
+    geom_col(mapping = aes(x=Symbol, y=max_mkt_cap, color = Symbol,
+                           fill = Symbol)) +
+    xlab("Coin") +
+    ylab("Max Market Cap ($)") +
+    ggtitle("Max Market Cap by Coin") +
+      theme(axis.text.x=element_blank()))
+  
+  mkt_max
+}
+
+vol_bar_graph <- function(coins) {
+  max_vol_mktcap <- coins %>%
+    group_by(Name) %>%
+    mutate(max_vol = round(max(Volume), 2)) %>%
+    slice(1) %>%
+    select(Symbol, max_vol)
+  vol_max <- ggplotly(ggplot(data = max_vol_mktcap) +
+                        geom_col(mapping = aes(x=Symbol, y=max_vol, color = Symbol,
+                                               fill = Symbol)) +
+                        xlab("Coin") +
+                        ylab("Volume ($ Transacted Daily)") +
+                        ggtitle("Max Daily Volume by Coin") +
+                        theme(axis.text.x=element_blank()))
+  
+  vol_max
+}
+
+max_mkt <- mkt_bar_graph(all_coins)
+max_vol <- vol_bar_graph(all_coins)
+
+
 
 
 
